6dc3c3ab960b64d139a75064285738e8f9a94821
biancachitu <bianca.ioana.chitu@gmail.com>
2025-04-11 17:29:21 +0300

    fixed docker-compose network

--- travelpoints_frontend/src/api.js
+++ travelpoints_frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const userApi = axios.create({
-    baseURL: "http://localhost:8081",
+    //baseURL: "http://localhost:8081",
+    baseURL: "http://localhost:80",
     headers: {
         "Content-Type": "application/json"
     }
