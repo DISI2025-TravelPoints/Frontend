4650693cc7790602a52263e8232e8287b648477f
foctavian <tavi.latavi121@gmail.com>
2025-04-27 08:24:15 +0300

    added ant design package for components

--- travelpoints_frontend/src/pages/HomeAdmin.js
+++ travelpoints_frontend/src/pages/HomeAdmin.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getAllAttractions } from '../requests/AdminRequests';
 
-function HomeAdmin() {
+import '../styles/HomeAdmin.css'; 
+const HomeAdmin = () => {
+
+    // based on the selected setting, we will show the corresponding component
+    const [selectedSetting, setSelectedSetting] = useState("attractions");
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+        switch(selectedSetting){
+            case "users":
+                break;
+            case "attractions":
+                setData(getAllAttractions());
+                break;
+            case "reviews":
+                break;
+        }
+    },[selectedSetting]);
+
     return (
-        <div style={{ padding: '20px', fontFamily: 'Poppins, sans-serif' }}>
-            <h1>Welcome, Admin!</h1>
-
-        </div>
+        <>
+            <div className="admin-sidebar-container">
+                <div className="admin-sidebar">
+                    <h2>Admin Menu</h2>
+                    <ul>
+                        <hr></hr>
+                        <button className = "admin-button"
+                        onClick={() => setSelectedSetting("users")}>
+                            Manage Users
+                        </button>
+                        <hr></hr>
+                        <button
+                         className = "admin-button"
+                         onClick={() => setSelectedSetting("attractions")}>
+                            Manage Attractions
+                        </button>
+                        <hr></hr>
+                        <button className = "admin-button"
+                         onClick={() => setSelectedSetting("reviews")}>
+                            Manage Reviews
+                        </button>
+                        <hr></hr>
+                    </ul>
+                </div>
+               
+            </div>
+            <div className="admin-content">
+            </div>
+        </>
     );
 }
 
---
+++ travelpoints_frontend/src/requests/AdminRequests.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API_DEPLOY_URL } from "../api";
+import { API_LOCAL_URL } from "../api";
+const ATTRACTIONS_API = "/api/attraction";
+
+export async function getAllAttractions() {
+    try {
+        let res = await axios.get("http://localhost/api/attraction", {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            withCredentials: true
+        });
+
+        console.log(res);
+        return res.data;
+    } catch (error) {
+        console.error('Error fetching attractions:', error);
+    }
+}
